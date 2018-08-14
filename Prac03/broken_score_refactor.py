f966395812cf1eb2a1f5910e56ec513722a9972b
Luke Sexton <luke.sexton@my.jcu.edu.au>
2018-08-14 21:52:57 +1000

    Add refactored scores function.

--- Prac03/broken_score_refactor.py
+++ Prac03/broken_score_refactor.py
@@ -3,15 +3,22 @@ CP1404/CP5632 - Practical - Suggested Solution
 Fixed program to determine score status
 """
 
-# Note boundary conditions (50 should be a pass, not > 50)
-# Note efficiency and nesting; use the fewest number of if/elif as possible
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+
+    score = float(input("Enter score: "))
+    scores(score)
+
+
+def scores(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
