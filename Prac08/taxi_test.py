5b343a337b74eb1c1fc29f95fd7ee14a34b6cba0
Luke <luke.sexton@my.jcu.edu.au>
2018-10-02 12:39:34 +1000

    Add new taxi with calculations on overall fares.

---
+++ Prac08/taxi_test.py
@@ -0,0 +1,12 @@
+from Prac08.taxi import Taxi
+
+new_taxi = Taxi("Prius 1", 100)
+print(new_taxi)
+new_taxi.start_fare()
+new_taxi.drive(40)
+print(new_taxi)
+print(new_taxi.get_fare())
+new_taxi.start_fare()
+new_taxi.drive(100)
+print(new_taxi)
+print(new_taxi.get_fare())
