c0823b24b74e44d3e7bc7a5e88984a5c7bb00892
Luke <luke.sexton@my.jcu.edu.au>
2018-10-23 13:27:08 +1000

    Finish Tasks one and two

--- Prac10/testing.py
+++ Prac10/testing.py
@@ -9,20 +9,22 @@ from Prac06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    if n == 1:
+        return s * n
+    return " ".join([s] * n)
 
 
-def is_long_word(word, length=5):
-    """
-    Determine if the word is as long or longer than the length passed in
-    >>> is_long_word("not")
-    False
-    >>> is_long_word("supercalifrag")
-    True
-    >>> is_long_word("Python", 6)
-    True
-    """
-    return len(word) > length
+# def is_long_word(word, length=5):
+#     """
+#     Determine if the word is as long or longer than the length passed in
+#     >>> is_long_word("not")
+#     False
+#     >>> is_long_word("supercalifrag")
+#     True
+#     >>> is_long_word("Python", 6)
+#     True
+#     """
+#     return len(word) > length
 
 
 def run_tests():
@@ -32,7 +34,6 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -40,12 +41,13 @@ def run_tests():
     # this should pass (no output)
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
+    assert test_car.fuel == 0
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10
 
 
 run_tests()
