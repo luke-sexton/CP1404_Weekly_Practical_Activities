fd46338604ad0bbd457e6cddf188244ba4ab951e
Luke <luke.sexton@my.jcu.edu.au>
2018-12-21 14:14:32 +1000

    utilise constants and fix concatenations when displaying prompts

--- Prac02/Extension_Work/ascii_table.py
+++ Prac02/Extension_Work/ascii_table.py
@@ -1,11 +1,16 @@
+LOWER = 33
+UPPER = 127
+
 user_character = input("Enter a character (it is case sensitive): ")
 print("The ASCII code for", user_character, "is", ord(user_character))
 
-user_number = int(input("Enter a number between 33 and 127: "))
+user_number = int(input("Enter a number between " + str(LOWER) + " and " + str(UPPER) + ": "))
+
 valid_input = False
 while not valid_input:
-    if 33 <= user_number <= 127:
+    if LOWER <= user_number <= UPPER:
         print("The character for", user_number, "is", chr(user_number))
         valid_input = True
     else:
-        user_number = int(input("Enter a number between 33 and 127: "))
+        user_number = int(input("Enter a number between " + str(LOWER) + " and " + str(UPPER) + ": "))
+
